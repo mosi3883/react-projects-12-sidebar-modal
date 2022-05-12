fca7ae60e59379f2562c5a5838e156c06528520d
Mostafa <mosi3883@gmail.com>
2022-05-12 13:06:28 +0430

    update to react@18.1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-ReactDOM.render(
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </React.StrictMode>
+);
